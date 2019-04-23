95c2faa6610aaabede3191d3a64f88c49b7b57ac
HoagyC <hoagycunningham@gmail.com>
2019-04-23 18:55:47 +0100

    adds tensorboard log, removes old metrics stuff (#15)

--- main.py
+++ main.py
@@ -55,46 +55,24 @@ def main():
     # Create and wrap the environment
     logging.info('Starting {env_name}'.format(**cfg))
     env = make_atari_env(env_id=cfg['env_name'], num_env=1, seed=cfg['train_seed'])
-
     env = VecFrameStack(env, n_stack=4)
     env = VecNormalize(env)
-    old_fn = env.step
-    mb_values = []
-
-    def new_fn(self, *args, **kwargs):
-        xs = old_fn(self, *args, **kwargs)
-        mb_values.append(xs[1])  # This is the reward as given by the
-        return xs
-
-    env.step = new_fn
 
     # Setting all known random seeds (Python, Numpy, TF, Gym if available)
     set_global_seeds(cfg['train_seed'])
 
     logging.info('Running {algo}'.format(**cfg))
-    model = ALGOS_DICT[cfg['algo']](policy=cfg['policy_type'], env=env)
+    model = ALGOS_DICT[cfg['algo']](
+        policy=cfg['policy_type'],
+        env=env,
+        tensorboard_log='./logs/{env_name}'.format(**cfg)
+    )
     model.verbose = 1
-
-    model = set_model_seed(model, cfg['train_seed'])
 
     logging.info('Training for {time_steps} steps'.format(**cfg))
 
-    # Loading file if available
-
-    # Logging metric
-    mb_averages = []
-
-    def callback(locals, globals):
-        nonlocal mb_values
-        # print(locals['self'].env.__dict__.keys())
-        if len(mb_values) >= 100:
-            mb_averages.append((sum(mb_values[:100]) / 100))
-            mb_values = mb_values[100:]
-
     # Training
-    model.learn(total_timesteps=cfg['time_steps'], log_interval=100, callback=callback)
-
-    print(mb_averages)
+    model.learn(total_timesteps=cfg['time_steps'], log_interval=100)
 
     # Saving
     logging.info('Saving model and metrics')
@@ -106,18 +84,14 @@ def main():
 
     with (save_dir / (base_name + '.txt')).open('w+') as f:
         f.write('%s\n' % json.dumps(cfg))
-        f.write(', '.join(str(x[0]) for x in mb_averages))
 
-    # Plotting performace
-    # plt.plot(list(range(len(mb_averages))), mb_averages)
-    # plt.show()
-
-    # Displaying gameplay
-    obs = env.reset()
-    while True:
-        action, _states = model.predict(obs)
-        obs, rewards, dones, info = env.step(action)
-        env.render()
+    if cfg['enjoy']:
+        # Displaying gameplay
+        obs = env.reset()
+        while True:
+            action, _states = model.predict(obs)
+            obs, rewards, dones, info = env.step(action)
+            env.render()
 
 
 if __name__ == '__main__':
