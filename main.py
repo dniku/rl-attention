3bd69fc8f020101696fde3ec9a7a3696ee8fecae
Lionel Miller <delonorm@gmail.com>
2019-05-03 02:40:34 +0200

    Move algorithm selection to algos.py

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from datetime import datetime
 from pathlib import Path
 
 import tensorflow as tf
-from stable_baselines import A2C, ACER, ACKTR, DQN, DDPG, PPO1, PPO2, SAC, TRPO
 from stable_baselines.common.cmd_util import make_atari_env
 from stable_baselines.common.vec_env import VecFrameStack
 from stable_baselines.common.vec_env import VecNormalize
 from stable_baselines.logger import configure
 from tqdm.auto import tqdm
 
+from algos import get_algo
 from losses import get_loss
 from models import get_network_builder
 
@@ -38,18 +38,6 @@ def set_model_seed(model, seed):
     return model
 
 
-# All available training algorithms
-ALGOS_DICT = {
-    'a2c': A2C,
-    'acer': ACER,
-    'acktr': ACKTR,
-    'dqn': DQN,
-    'ddpg': DDPG,
-    'ppo1': PPO1,
-    'ppo2': PPO2,
-    'sac': SAC,
-    'trpo': TRPO,
-}
 class Callback(object):
     def __init__(self, output_dir):
         self.output_dir = output_dir
@@ -109,7 +97,7 @@ def main(cfg, run_dir):
     set_global_seeds(cfg['train_seed'])
 
     logging.info('Running {algo}'.format(**cfg))
-    model = ALGOS_DICT[cfg['algo']](
+    model = get_algo(cfg['algo'])(
         policy=cfg['policy_type'],
         env=env,
         verbose=1,
